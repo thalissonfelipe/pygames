47cad15162be81d418529524d597b2e6c0f81b4f
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-10-21 20:13:18 -0300

    feat: add camera to the player

--- platform/level.py
+++ platform/level.py
@@ -2,7 +2,7 @@ import pygame
 
 from tiles import Tile
 from player import Player
-from settings import tile_size
+from settings import tile_size, screen_width
 
 
 class Level:
@@ -23,8 +23,24 @@ class Level:
                 if col == 'P':
                     self.player.add(Player(pos))
 
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+
+        if player_x < (screen_width / 4) and direction_x < 0:
+            self.world_shift = 8
+            player.speed = 0
+        elif player_x > screen_width - (screen_width / 4) and direction_x > 0:
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
+
     def run(self):
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
         self.player.update()
         self.player.draw(self.display_surface)
+        self.scroll_x()
