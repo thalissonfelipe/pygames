5d0a39342990a0877396734a179119cc0a131936
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-10-24 11:04:55 -0300

    feat: add dust particles animations

--- platform/level.py
+++ platform/level.py
@@ -2,15 +2,44 @@ import pygame
 
 from tiles import Tile
 from player import Player
+from particles import ParticleEffect
 from settings import tile_size, screen_width
 
 
 class Level:
     def __init__(self, level_data, surface):
+        # level setup
         self.display_surface = surface
         self.setup_level(level_data)
         self.world_shift = 0
         self.current_x = 0
+
+        # dust
+        self.dust_sprite = pygame.sprite.GroupSingle()
+        self.player_on_ground = False
+
+    def create_jump_particles(self, pos):
+        if self.player.sprite.facing_right:
+            pos -= pygame.math.Vector2(10, 5)
+        else:
+            pos += pygame.math.Vector2(10, -5)
+        jump_particle_sprite = ParticleEffect(pos, 'jump')
+        self.dust_sprite.add(jump_particle_sprite)
+
+    def get_player_on_ground(self):
+        if self.player.sprite.on_ground:
+            self.player_on_ground = True
+        else:
+            self.player_on_ground = False
+
+    def create_landing_dust(self):
+        if not self.player_on_ground and self.player.sprite.on_ground and not self.dust_sprite.sprites():
+            if self.player.sprite.facing_right:
+                offset = pygame.math.Vector2(10, 15)
+            else:
+                offset = pygame.math.Vector2(-10, 15)
+            fall_dust_particle = ParticleEffect(self.player.sprite.rect.midbottom - offset, 'land')
+            self.dust_sprite.add(fall_dust_particle)
 
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
@@ -22,7 +51,7 @@ class Level:
                 if col == 'X':
                     self.tiles.add(Tile(pos, tile_size))
                 if col == 'P':
-                    self.player.add(Player(pos))
+                    self.player.add(Player(pos, self.display_surface, self.create_jump_particles))
 
     def scroll_x(self):
         player = self.player.sprite
@@ -80,11 +109,19 @@ class Level:
             player.on_ceiling = False
 
     def run(self):
+        # dust particles
+        self.dust_sprite.update(self.world_shift)
+        self.dust_sprite.draw(self.display_surface)
+
+        # level tiles
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
         self.scroll_x()
 
+        # player
         self.player.update()
         self.horizontal_movement_collision()
+        self.get_player_on_ground()
         self.vertical_movement_collision()
+        self.create_landing_dust()
         self.player.draw(self.display_surface)
--- platform/player.py
+++ platform/player.py
@@ -5,13 +5,20 @@ from support import import_folder
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos):
+    def __init__(self, pos, surface, create_jump_particles):
         super().__init__()
         self.import_character_assets()
         self.frame_index = 0
         self.animation_speed = 0.15
         self.image = self.animations['idle'][self.frame_index]
         self.rect = self.image.get_rect(topleft=pos)
+
+        # dust particles
+        self.import_dust_run_particles()
+        self.dust_frame_index = 0
+        self.dust_animation_speed = 0.15
+        self.display_surface = surface
+        self.create_jump_particles = create_jump_particles
 
         # player movement
         self.direction = pygame.math.Vector2(0, 0)
@@ -34,6 +41,9 @@ class Player(pygame.sprite.Sprite):
         for animation in self.animations.keys():
             full_path = os.path.join(character_path, animation)
             self.animations[animation] = import_folder(full_path)
+
+    def import_dust_run_particles(self):
+        self.dust_run_particles = import_folder('assets/character/dust_particles/run')
 
     def animate(self):
         animation = self.animations[self.status]
@@ -63,6 +73,22 @@ class Player(pygame.sprite.Sprite):
             else:
                 self.rect = self.image.get_rect(midtop=self.rect.midtop)
 
+    def run_dust_animation(self):
+        if self.status == 'run' and self.on_ground:
+            self.dust_frame_index += self.dust_animation_speed
+            if self.dust_frame_index >= len(self.dust_run_particles):
+                self.dust_frame_index = 0
+
+            dust_particle = self.dust_run_particles[int(self.dust_frame_index)]
+
+            if self.facing_right:
+                pos = self.rect.bottomleft - pygame.Vector2(6, 10)
+                self.display_surface.blit(dust_particle, pos)
+            else:
+                pos = self.rect.bottomright - pygame.Vector2(6, 10)
+                flipped_dust_particle = pygame.transform.flip(dust_particle, True, False)
+                self.display_surface.blit(flipped_dust_particle, pos)
+
     def get_input(self):
         keys = pygame.key.get_pressed()
 
@@ -77,6 +103,7 @@ class Player(pygame.sprite.Sprite):
 
         if keys[pygame.K_SPACE] and self.on_ground:
             self.jump()
+            self.create_jump_particles(self.rect.midbottom)
 
     def get_status(self):
         if self.direction.y < 0:
@@ -100,3 +127,4 @@ class Player(pygame.sprite.Sprite):
         self.get_input()
         self.get_status()
         self.animate()
+        self.run_dust_animation()
